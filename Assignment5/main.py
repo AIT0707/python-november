b9f10dd01da39504f2f52d92aae66a581a58d3cf
Ikrom Abdullaev <ikrom.abdullaev@ucentralasia.org>
2023-11-20 17:03:54 +0500

    Created Assignment5 folder

---
+++ Assignment5/main.py
@@ -0,0 +1,28 @@
+# Code 1
+if 10 > 5:
+print("Ten is greater than five!")
+#It gives error because of absence of the identation in the third line. Below the correct rewrited code snippet.
+if 10>5:
+    print ("Ten is greater than five!")
+
+# Code 2
+if 8 > 3:
+    print("Eight is greater than three!")
+    print("Indeed!")
+# The Code 2 snippet is correct as both print lines here started with the same number of spaces. 
+
+# Code 3
+a = 7
+b = 3
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+# The Code 3 snippet is correct as in each blocks the print lines have identations. 
+
+"""
+Indentation is a very important concept in Python because it defines the grouping of codes. 
+True indentation is necessary to avoid syntax errors and ensure code works correctly. 
+Unlike other programming languages that use curly braces, parentheses, or keywords to separate blocks of code, 
+Python uses indentation to highlight them. This approach make a code easy to read and simplifies code structure.
+"""
