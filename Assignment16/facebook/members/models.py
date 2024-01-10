f3806f05bcad478302171c23e9002675e872f853
Ikrom Abdullaev <ikrom.abdullaev@ucentralasia.org>
2024-01-10 19:07:42 +0500

    Folder Assignment16 updated

--- Assignment16/facebook/members/models.py
+++ Assignment16/facebook/members/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Cities(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    added_date = models.DateField(null=True)
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    added_date = models.DateField(null=True)
+
+class Friends(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    added_date = models.DateField(null=True)
